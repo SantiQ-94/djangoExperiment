6c013b1e12798b963cf6426151d8b631d61e49b9
Sanqui.94 <san.quit@hotmail.com>
2018-09-25 11:08:58 -0400

    more django girls tuto

--- blog/models.py
+++ blog/models.py
@@ -10,7 +10,7 @@ class Post(models.Model):
             default=timezone.now
     )
     published_date = models.DateTimeField(
-            blank=True, null=False
+            blank=True, null=True
     )
     
     def publish(self):
